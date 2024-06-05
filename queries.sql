6b5756fd288af556e1fdce18c93dadb2aed5d356
Kirill Yelskas <97301835+fenrirg6@users.noreply.github.com>
2024-06-05 10:50:12 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -68,6 +68,18 @@ UPD: сортировка была некорректна, т.к. приводи
 - конвертация даты в дни недели в ISO-формате.
 */
 
+SELECT
+	CONCAT(e.first_name,' ', e.last_name) AS seller,
+	to_char(s.sale_date, 'day') AS day_of_week,
+	FLOOR(sum(p.price * s.quantity)) AS income
+FROM sales s
+	LEFT JOIN employees e
+ON e.employee_id = s.sales_person_id 
+	LEFT JOIN products p
+ON s.product_id = p.product_id
+GROUP BY seller, day_of_week, to_char(s.sale_date, 'ID')
+ORDER BY to_char(s.sale_date, 'ID'), seller
+
 --5
 /*
 Через CASE прописываем условие для дифференциации покупателей по возрастным
@@ -75,6 +87,17 @@ UPD: сортировка была некорректна, т.к. приводи
 считаем количество строк при группироке по возрастной
 группе.
 */
+
+SELECT
+    CASE
+        WHEN age BETWEEN 16 AND 25 THEN '16-25'
+        WHEN age BETWEEN 26 AND 40 THEN '26-40'
+        ELSE '40+'
+    END AS age_category,
+    COUNT(*) AS age_count
+FROM customers
+GROUP BY age_category
+ORDER BY age_category
 
 --6
 /*
@@ -85,8 +108,52 @@ UPD: сортировка была некорректна, т.к. приводи
 с округлением до целых.
 */
 
+SELECT
+    TO_CHAR(s.sale_date, 'YYYY-MM') AS selling_month,
+    COUNT(DISTINCT s.customer_id) AS total_customers,
+    FLOOR(SUM(p.price * s.quantity)) AS income
+FROM sales AS s
+LEFT JOIN customers AS c
+    ON s.customer_id = c.customer_id
+LEFT JOIN products AS p
+    ON s.product_id = p.product_id
+GROUP BY selling_month
+ORDER BY selling_month ASC
+
 --7
 /*
 Оконными функциями ищем "первые вхождения" промоционных товаров и соотв. дату.
 Обертка в CTE для удобства написания запроса :)
 */
+
+WITH initial_table AS (
+    SELECT
+        CONCAT(c.first_name, ' ', c.last_name) AS customer,
+        FIRST_VALUE(s.sale_date)
+            OVER (PARTITION BY c.customer_id ORDER BY s.sale_date)
+        AS sale_date,
+        CONCAT(e.first_name, ' ', e.last_name) AS seller,
+        FIRST_VALUE(p.price)
+            OVER (
+                PARTITION BY c.customer_id ORDER BY s.sale_date, c.customer_id
+            )
+        AS first_val_disc
+    FROM customers AS c
+    LEFT JOIN sales AS s
+        ON c.customer_id = s.customer_id
+    LEFT JOIN employees AS e
+        ON s.sales_person_id = e.employee_id
+    LEFT JOIN products AS p
+        ON s.product_id = p.product_id
+),
+
+first_val_disc_table AS (
+    SELECT DISTINCT ON (customer)
+        customer,
+        sale_date,
+        seller
+    FROM initial_table
+    WHERE first_val_disc = 0
+)
+
+SELECT * FROM first_val_disc_table
